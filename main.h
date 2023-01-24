a308af95af8c672740c1f96c4b586022312a2195
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-24 12:00:53 +0300

    c file

--- main.h
+++ main.h
@@ -4,41 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 #include <unistd.h>
-#include <limits.h>
-/**
- * struct type - connect conversion specifiers with the correct print function
- * @print: a function pointer for the print functions
- * @identifier: the conversion specifier
- */
-typedef struct type
-{
-	char *hm;
-	int (*print)(va_list);
-} type_t;
 
-int print_pe(va_list args);
+/**
+ * struct types - specifier structure for printf
+ * @p: pointer to characteres specifiers
+ * @func : function pointer to print fucntions
+ */
+typedef struct types
+{
+	char p;
+	int (*func)(va_list);
+} print_f;
+
 int _putchar(char c);
-int (*get_function(const char *specifier))(va_list);
-int _printf(const char *, ...);
-int _strlen(char *);
-int print_rev(va_list args);
-int rot13(va_list args);
-int print_number(unsigned int n);
-int countDigits(unsigned int num);
-int countOctal(unsigned int num);
-int countBinary(unsigned int num);
-int print_b(va_list args);
-int print_o(va_list args);
-int print_d(va_list args);
-int print_x(va_list args);
-void print_lowerHex(unsigned int num, int *count);
-int print_X(va_list args);
-void print_upperHex(unsigned int num, int *count);
-int print_p(va_list args);
-int print_s(va_list args);
-int print_c(va_list args);
-int print_u(va_list args);
-int print_F(va_list args);
+int _printf(const char *format, ...);
+int printc(va_list l);
+int print_string(va_list s);
+int (*cmp_func(const char a))(va_list);
+int print_n(va_list n);
 
 #endif /* MAIN_H */

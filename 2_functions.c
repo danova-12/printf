07716d917807d63d097df706ca8b65b0e6762e73
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-25 20:56:08 +0300

    c file

---
+++ 2_functions.c
@@ -0,0 +1,98 @@
+/**
+ * print_x - print hexadecimal numbers
+ * in lowercase
+ * @x: input parameter
+ *
+ * Return: number of bits
+ */
+int print_x(va_list x)
+{
+	int counter = 0, j;
+	unsigned int array[8];
+	unsigned int div;
+
+	div = va_arg(x, int);
+	if (div == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (div > 0)
+	{
+		array[counter] = div % 16;
+		div = div / 16;
+		counter++;
+	}
+	for (j = counter - 1; j >= 0; j--)
+	{
+		if (array[j] <= 9)
+			_putchar('0' + array[j]);
+		else
+			_putchar('a' + array[j] - 10);
+	}
+	return (counter);
+}
+/**
+ * print_X - print hexadecimal numbers
+ * in uppercase
+ * @X: input parameter
+ *
+ * Return: number of bits
+ */
+int print_X(va_list X)
+{
+	int counter = 0, j;
+	unsigned int array[8];
+	unsigned int div;
+
+	div = va_arg(X, int);
+	if (div == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (div > 0)
+	{
+		array[counter] = div % 16;
+		div = div / 16;
+		counter++;
+	}
+	for (j = counter - 1; j >= 0; j--)
+	{
+		if (array[j] <= 9)
+			_putchar('0' + array[j]);
+		else
+			_putchar('A' + array[j] - 10);
+	}
+	return (counter);
+}
+/**
+ * print_o - print octal numbers
+ * @b: input parameter
+ *
+ * Return: number of bits
+ */
+int print_o(va_list o)
+{
+	int counter = 0, j;
+	unsigned int array[11];
+	unsigned int div;
+
+	div = va_arg(o, int);
+	if (div == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (div > 0)
+	{
+		array[counter] = div % 8;
+		div = div / 8;
+		counter++;
+	}
+	for (j = counter - 1; j >= 0; j--)
+	{
+		_putchar('0' + array[j]);
+	}
+	return (counter);
+}
